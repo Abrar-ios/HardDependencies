e7fe5709e3d2a108ca75c8324ac475728d264df6
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-14 17:39:59 +0500

    Practice substitute singleton and add test suit for singleton

---
+++ HardDependenciesTests/MySingletonViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  MySingletonViewControllerTests.swift
+//  HardDependenciesTests
+//
+//  Created by HAQQQABD on 14/08/2023.
+//
+
+import XCTest
+@testable import HardDependencies
+
+final class MySingletonViewControllerTests: XCTestCase {
+    func test_zero(){
+        XCTFail("There is no test implemented in MySingletonViewControllerTests class yet")
+    }
+}
