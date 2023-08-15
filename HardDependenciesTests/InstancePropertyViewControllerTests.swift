e8d503e082d966e1ebba97ea4804acaf8ad813b2
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-16 00:01:36 +0500

    Add InstancePropertyViewControllerTests suit
    implement test_zero to confirm communication between InstancePropertyViewController and InstancePropertyViewControllerTests suit

---
+++ HardDependenciesTests/InstancePropertyViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  InstancePropertyViewControllerTests.swift
+//  HardDependenciesTests
+//
+//  Created by HAQQQABD on 15/08/2023.
+//
+
+import XCTest
+@testable import HardDependencies
+
+final class InstancePropertyViewControllerTests:XCTestCase {
+    func test_zero(){
+        XCTFail("There is no tests implemented in InstancePropertyViewControllerTests yet")
+    }
+}
