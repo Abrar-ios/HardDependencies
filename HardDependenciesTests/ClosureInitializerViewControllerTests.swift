762f33da15d2357f77aeef716def84b618bb9bfd
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-16 23:25:16 +0500

    Add ClosureInitializerViewControllerTests suit
    .Implement test_zero to confirm communication between ClosureInitializerViewControllerTests and ClosureInitializerViewController

---
+++ HardDependenciesTests/ClosureInitializerViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  ClosureInitializerViewControllerTests.swift
+//  HardDependenciesTests
+//
+//  Created by HAQQQABD on 16/08/2023.
+//
+
+import XCTest
+@testable import HardDependencies
+
+final class ClosureInitializerViewControllerTests: XCTestCase {
+    func test_zero(){
+        XCTFail("There is no test implemented in ClosureInitializerViewControllerTests class yet")
+    }
+}
