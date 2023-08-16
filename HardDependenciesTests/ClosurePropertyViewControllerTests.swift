3af3a18a7523d32eb6b7c23f7fdb1dee7484a503
Abrar Ul Haq <abrarulhaq.ios@gmail.com>
2023-08-16 23:36:37 +0500

    Add ClosurePropertyViewControllerTests suit
    Implemented test_zero to confirm communication between ClosurePropertyViewControllerTests and ClosurePropertyViewController

---
+++ HardDependenciesTests/ClosurePropertyViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  ClosurePropertyViewControllerTests.swift
+//  HardDependenciesTests
+//
+//  Created by HAQQQABD on 16/08/2023.
+//
+
+import XCTest
+@testable import HardDependencies
+
+final class ClosurePropertyViewControllerTests: XCTestCase {
+    func test_zero(){
+        XCTFail("There is no tests implemented in ClosurePropertyViewControllerTests suit yet")
+    }
+}
